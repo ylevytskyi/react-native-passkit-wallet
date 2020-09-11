d8942a30bb17369c87123116a147c68acb78dcd2
ylevytskyi <33432306+ylevytskyi@users.noreply.github.com>
2020-09-11 11:32:55 +0300

    Update RNPassKitModule.java
    Fix for react-native 0.63.2

--- android/src/main/java/com/mybdesign/RNPassKit/RNPassKitModule.java
+++ android/src/main/java/com/mybdesign/RNPassKit/RNPassKitModule.java
@@ -3,7 +3,7 @@ package com.mybdesign.RNPassKit;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.util.Base64;
 
 import com.facebook.react.bridge.JSApplicationCausedNativeException;
